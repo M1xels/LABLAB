fba75ac28e6ef63664e214696640ebdd1e4bd71b
M1xels <micoryan.sanpablo@gsfe.tupcavite.edu.ph>
2022-05-16 00:53:42 +0800

    register

--- Ourproject/Labapp/static/JS/reg.js
+++ Ourproject/Labapp/static/JS/reg.js
@@ -1,24 +1,38 @@
-function register(){
-
-    var f1  = document.getElementById("Fullname");
-    var p1  = document.getElementById("password");
-    var em1  = document.getElementById("em");
-    var n1  = document.getElementById("num1");
-    var c1  = document.getElementById("com");
-    var s1  = document.getElementById("staf");
-
-    if ( f1.value == "" || n1.value == "" || p1.value == "" || em1.value == "" || s1.checked == false && c1.checked == false){
-        alert("please complete the fill up");
-        
-    } 
-    else{
-        window.location="{% url 'homepage' %}";
-        document.getElementById("Fullname").value ='';
-        document.getElementById("password").value ='';
-        document.getElementById("em").value ='';
-        document.getElementById("com").checked ='';
-        document.getElementById("staf").checked ='';
-    }
+function toggle(){
+    var x = document.getElementById("form1");
+    var y = document.getElementById("form2");
+    var z = document.getElementById("form3");
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
     
+}
+function toggle1(){
+    var x = document.getElementById("form1");
+    var y = document.getElementById("form2");
+    var z = document.getElementById("form3");
+    x.style.display = "none";
 
+    y.style.display = "none";
+    z.style.display = "block";
+    
+}
+function back(){
+    var x = document.getElementById("form1");
+    var y = document.getElementById("form2");
+    var z = document.getElementById("form3");
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+    
+}
+function back1(){
+    var x = document.getElementById("form1");
+    var y = document.getElementById("form2");
+    var z = document.getElementById("form3");
+    x.style.display = "none";
+
+    y.style.display = "block";
+    z.style.display = "none";
+    
 }
